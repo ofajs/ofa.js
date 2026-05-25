7beb21ed7ff75ba1a58c1ddbd6fcddaa50173f7e
Yao <kirayao@hotmail.com>
2026-05-25 15:54:58 +0800

    feat: improve render error handling

--- packages/xhear/render/render.mjs
+++ packages/xhear/render/render.mjs
@@ -89,13 +89,20 @@ export function render({
       const matchs = Array.from(new Set(originStyle.match(/data\(.+?\)/g))).map(
         (dataExpr) => {
           const expr = dataExpr.replace(/data\((.+)\)/, "$1");
-          const func = convertToFunc(expr, data);
+          const func = convertToFunc(expr, data, {
+            errCall: (error) => {
+              console.error(
+                `Error evaluating data() expression in style: "${expr}", style: "${originStyle}", target:`,
+                target,
+              );
+            },
+          });
 
           return {
             dataExpr,
             func,
           };
-        }
+        },
       );
 
       const renderStyle = () => {
@@ -130,7 +137,16 @@ export function render({
     parentNode.insertBefore(textEl, el);
     parentNode.removeChild(el);
 
-    const func = convertToFunc(el.getAttribute("expr"), data);
+    const func = convertToFunc(el.getAttribute("expr"), data, {
+      errCall: (error) => {
+        console.error(
+          `Error evaluating text expression: "${el.getAttribute("expr")}", element:`,
+          textEl,
+          `parent:`,
+          parentNode,
+        );
+      },
+    });
     const renderFunc = () => {
       const content = func();
       if (textEl.textContent !== String(content)) {
@@ -173,13 +189,13 @@ export function render({
 
             const func = convertToFunc(expr, data, {
               errCall: (error) => {
-                const errorExpr = `:${key}="${expr}"`;
+                const errorExpr = `${actionName === "prop" ? "" : actionName}:${key}="${expr}"`;
                 const err = getErr(
                   "render_el_error",
                   {
                     expr: errorExpr,
                   },
-                  error
+                  error,
                 );
 
                 console.warn(err, {
@@ -251,7 +267,7 @@ export function render({
               arg0: args[0],
               arg1: args[1],
             },
-            error
+            error,
           );
           console.warn(err, el);
           throw err;
@@ -372,7 +388,7 @@ export const convert = (template) => {
     /{{(.+?)}}/g,
     (str, match) => {
       return `<xtext expr="${match}"></xtext>`;
-    }
+    },
   );
 
   const tempName = template.getAttribute("name");
@@ -395,7 +411,7 @@ export const convert = (template) => {
           tempName,
           len: tempChilds.length,
           wrapName,
-        })
+        }),
       );
     }
     temps[tempName] = template;
